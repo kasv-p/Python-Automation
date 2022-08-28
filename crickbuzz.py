2b28e7adfe1b08a70852d3b69046e41ba469516c
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-08-28 22:45:30 +0530

    Update crickbuzz.py

--- crickbuzz.py
+++ crickbuzz.py
@@ -3,7 +3,7 @@ import bs4
 from plyer import notification
 import time
 
-link='https://www.cricbuzz.com/live-cricket-scores/46081/srh-vs-gt-40th-match-indian-premier-league-2022'
+link='https://www.cricbuzz.com/live-cricket-scores/51210/2nd-match-group-a-asia-cup-2022'
 
 start=time.time()
 
@@ -18,9 +18,21 @@ while True:
 	for i in details:
 
 		# print(i.getText())
-		notification.notify(title='Cricket Score',message=i.getText(),app_icon='a.ico',timeout=5)
+		notification.notify(title='Cricket Score',message=i.getText(),app_icon='cricket_logo.ico',timeout=5)
 	
 	if time.time()-start > 300:
 		break
 	time.sleep(100)
 print('DONE')
+
+# import requests
+# import bs4
+# from plyer import notification
+#
+# link='https://www.cricbuzz.com/cricket-series/4061/indian-premier-league-2022/points-table'
+#
+#
+# res=requests.get(link)
+#
+# soup=bs4.BeautifulSoup(res.text,'html.parser') # entire source code of website
+# print(soup)
